13fe701d4c8842d4bfe80f252e25b1eb749c77c8
Soumen Samanta <soumensamanta578@gmail.com>
2026-07-25 17:38:46 +0530

    remove venv from tracking

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-# build.sh
-
-#!/usr/bin/env bash
-
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+# venv folder remove করো GitHub থেকে
+echo "venv/" >> .gitignore
+git rm -r --cached venv/
+git add .
+git commit -m "remove venv from tracking"
+git push
